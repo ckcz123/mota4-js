1d0cc1a25de1fc4cfb3672bd7e37736966bc5e6c
oc <ckcz123@126.com>
2017-11-25 00:05:06 +0800

    Fix Bug

--- libs/core.js
+++ libs/core.js
@@ -345,6 +345,8 @@ core.prototype.loadSound = function() {
 core.prototype.loadSoundItem = function (toLoadList) {
     if (toLoadList.length==0) {
         // console.log('play bgm..');
+        core.drawTip('播放BGM');
+        alert('播放BGM');
         if (core.musicStatus.soundStatus)
             core.playBgm('bgm', 'mp3');
         return;
